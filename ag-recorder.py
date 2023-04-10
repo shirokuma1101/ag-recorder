12dd3c514cf2834586f29f87616e77dc80830eda
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2023-04-11 04:32:31 +0900

    feat: アプリケーションを一部実装

--- ag-recorder.py
+++ ag-recorder.py
@@ -1,8 +1,22 @@
-#import
+# standard
+
+# wx
+import wx
+
+#
+from agrecorder.agpg import AGPG
+from agrecorder.agrp import AGRP
+from agrecorder.agrw import AGRW
 
 
 def main():
-    pass
+    agpg = AGPG()
+    agrp = AGRP('https://hls-base1.mitene.ad.jp/agqr1/iphone/3Gs.m3u8', 'C:\\Workspace\\Tmp\\')
+
+    app = wx.App()
+    agrw = AGRW(agpg, agrp)
+    agrw.Show()
+    app.MainLoop()
 
 
 if __name__ == '__main__':
