de60504fcd3c1be104e3cd6b0f6363a294e6de60
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2023-04-15 03:48:34 +0900

    feat: iniの構成を変更、それに伴うソースの変更

--- ag-recorder.py
+++ ag-recorder.py
@@ -1,17 +1,19 @@
 # standard
+import configparser
 
-# wx
-import wx
-
-#
+# agpg, agrp, agrw
 from agrecorder.agpg import AGPG
 from agrecorder.agrp import AGRP
 from agrecorder.agrw import AGRW
 
 
 def main():
-    agpg = AGPG()
-    agrp = AGRP('https://hls-base1.mitene.ad.jp/agqr1/iphone/3Gs.m3u8', 'C:\\Workspace\\Tmp\\')
+    # config
+    config = configparser.ConfigParser()
+    config.read('ag-recorder.ini')
+
+    agpg = AGPG(config['SETTINGS']['AGPGS_DIR'])
+    agrp = AGRP(config['SETTINGS']['AG_HLS_URL'], config['SETTINGS']['FFMPEG_PATH'], config['SETTINGS']['OUTPUT_DIR'])
 
     agrw = AGRW(agpg, agrp)
     agrw.run()
